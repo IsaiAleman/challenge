97b8c3b726210e423e7583e18f5cc52f27bb42cf
IsaiAleman <jorge.isai.aleman19@gmail.com>
2024-11-29 23:09:53 -0600

    Se agregan las funciones para la q1

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -1,5 +1,28 @@
+import json
 from typing import List, Tuple
 from datetime import datetime
+from collections import defaultdict
 
 def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+    date_counts = defaultdict(int)
+    user_counts_per_date = defaultdict(lambda: defaultdict(int))
+
+    with open(file_path, 'r', encoding='utf-8') as f:
+        for line in f:
+            tweet = json.loads(line)
+            date_str = tweet['date'][:10]
+            date = datetime.strptime(date_str, '%Y-%m-%d').date()
+            username = tweet['user']['username']
+
+            date_counts[date] += 1
+            user_counts_per_date[date][username] += 1
+
+    top_dates = sorted(date_counts.items(), key=lambda x: x[1], reverse=True)[:10]
+
+    result = []
+    for date, _ in top_dates:
+        user_counts = user_counts_per_date[date]
+        top_user = max(user_counts.items(), key=lambda x: x[1])[0]
+        result.append((date, top_user))
+
+    return result
