2ad1e6870e32d906f6f0968c706df7cf8532b2c6
IsaiAleman <jorge.isai.aleman19@gmail.com>
2024-11-29 23:52:25 -0600

    Se agregan las funciones para la q2

--- src/q2_time.py
+++ src/q2_time.py
@@ -1,4 +1,46 @@
+import pandas as pd
+import re
+import orjson
 from typing import List, Tuple
 
 def q2_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    data = []
+    with open(file_path, 'rb') as f:
+        for line in f:
+            tweet = orjson.loads(line)
+            data.append({
+                'content': tweet['content'],
+            })
+
+    df = pd.DataFrame(data)
+
+    contents = df['content']
+
+    emoji_pattern = re.compile(
+        "["
+        "\U0001F600-\U0001F64F"  # Emoticons
+        "\U0001F300-\U0001F5FF"  # Symbols & Pictographs
+        "\U0001F680-\U0001F6FF"  # Transport & Map Symbols
+        "\U0001F1E0-\U0001F1FF"  # Flags
+        "\U00002702-\U000027B0"  # Dingbats
+        "\U000024C2-\U0001F251"
+        "]+",
+        flags=re.UNICODE
+    )
+
+    def extract_emojis(text):
+        return emoji_pattern.findall(text)
+
+    df['emojis'] = contents.apply(extract_emojis)
+
+    emojis_series = df['emojis'].explode()
+
+    emojis_series = emojis_series.dropna()
+
+    emoji_counts = emojis_series.value_counts()
+
+    top_emojis = emoji_counts.head(10)
+
+    result = list(top_emojis.items())
+
+    return result
